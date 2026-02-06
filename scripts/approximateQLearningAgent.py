94e201a34337dbf82ffdffdec81f09ab7b63b1a2
odpgit <owen_gr@premhome.net>
2026-02-06 01:45:34 -0500

    Continued work on AQL agent features, including data storage for joint graph, remaining destinations, and paths planned

--- scripts/approximateQLearningAgent.py
+++ scripts/approximateQLearningAgent.py
@@ -3,7 +3,7 @@ import networkx as nx
 
 #Question: how to handle storing feature weights?
 #   Options: include training function that must be run every time before actually running agent for gameplay
-#            include training function that only needs to be run once (and stores results with pickle to load in at __init__)
+#   DO THIS  include training function that only needs to be run once (and stores results with pickle to load in at __init__)
 #Question: which features to weigh?
 #             Which features are avilable in game class?
 #               - Board (graph)
@@ -23,6 +23,9 @@ import networkx as nx
 # findMaxWeightSumForNode function - pass in a player and start node, can calculate longest route
 #           Expensive - need to call for each start node. Store longest route for each player locally during game, update with moves?
 
+#Glenn: features involving the action - know what action each of the four agents would pick
+#       "this action scores points for me, completes card, claims route, picks useful traincard etc."
+
 #   TENTATIVE LIST
 #   (0) Number of players in game (if not fixed)
 #   (1) Minimum number of trains left among all players
@@ -31,6 +34,8 @@ import networkx as nx
 #   (4) Sum of points remaining to complete all destination cards in hand
 #   (5) TRACTABLE? Number of train car cards needed to complete any of the destination cards in hand
 #   (6) TRACTABLE? Number of face-up train car cards that let you complete a route needed for a destination card 
+#   (TODO): Current points compared to others
+#   (TODO): Wild cards available
 #   (7) Turn number
 
 #   General strategies of each agent
@@ -56,14 +61,34 @@ import networkx as nx
 class ApproximateQLearningAgent(Agent):
     def __init__(self, numPlayers):
         self.longest_route_by_player = [0] * numPlayers
-        self.remaining_destinations = []
+        self.jgraph = None
+        self.remaining_dest = []
+        self.paths_planned = {}
         self.turn_number = 0
+
+    def weight_function(zero_edges):
+        def fn(u, v, d):
+            if (u, v) in zero_edges or (v, u) in zero_edges:
+                return 0
+            return d.get("weight", 1)
+        return fn
     
     def decide(self, game, pnum):
-        #update longest route by player somehow
-
         #update turn number
         self.turn_number += 1
+
+        #update joint graph/remaining destinations storage
+        self.jgraph = self.joint_graph(game, pnum)
+        self.remaining_dest = self.destinations_not_completed(game, pnum)
+
+        #update paths planned with shortest available path to each remaining dcard
+        claimed_edges = set(game.player_graph(pnum).edges())
+        for d in self.remaining_dest:
+            self.paths_planned[d] = nx.shortest_path(self.jgraph, d['city1'], d['city2'], weight=self.weight_function(claimed_edges))
+
+        #update longest route by player somehow
+
+        #consult features to make decision (AFTER above updates)
 
     def feature_num_players(self, game, pnum):
         return game.number_of_players()
@@ -81,33 +106,37 @@ class ApproximateQLearningAgent(Agent):
         return overall_longest_route - own_longest_route + 1 #to avoid zeroing out in vector multiplication
 
     def feature_minimum_possible_dcard_paths(self, game, pnum):
-        jgraph = self.joint_graph(game, pnum)
-        rem_dest = self.destinations_not_completed(game, pnum, jgraph)
-        if len(rem_dest) == 0:
+        if len(self.remaining_dest) == 0:
             return 0 #zero out in vector multiplication
         
         min_poss_paths = float('inf')
-        for dcard in rem_dest:
+        for dcard in self.remaining_dest:
             #get possible paths to dcard
-            paths_generator = nx.all_simple_paths(jgraph, dcard.city1, dcard.city2)
+            paths_generator = nx.all_simple_paths(self.joint_graph, dcard.city1, dcard.city2)
             min_poss_paths = min(min_poss_paths, len(paths_generator))
         
         return min_poss_paths + 1 #to avoid zeroing out in vector multiplication
     
     def feature_dcard_points_remaining(self, game, pnum):
-        jgraph = self.joint_graph(game, pnum)
-        rem_dest = self.destinations_not_completed(game, pnum, jgraph)
         res = 0
-        for dcard in rem_dest:
+        for dcard in self.remaining_dest:
             res += dcard.points
-        
         return res
     
     def feature_num_traincar_cards_for_dcards_remaining(self, game, pnum):
-        pass
+        pass 
 
     def feature_faceup_cards_for_dcards_remaining(self, game, pnum):
-        pass
+        #game.train_cards_face_up
+        count = 0
+        for path in self.paths_planned:
+            pass
 
     def feature_turn_number(self, game, pnum):
         return self.turn_number
+    
+    def feature_current_points(self, game, pnum):
+        return game.players[pnum].points
+
+    def feature_wild_cards_available(self, game, pnum):
+        return game.train_cards_face_up["wild"]
